f37f15d8e10679c71523b92afd1aa51036933a05
Saltymeatball <hao100319@gmail.com>
2024-04-08 23:27:09 +0700

    adjust/ root.vue

--- src/Root.vue
+++ src/Root.vue
@@ -1,19 +1,4 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { useRoute } from 'vue-router';
-
-import LayoutDefault from './layouts/default.vue'
-import LayoutAuth from './layouts/auth.vue'
-
-const route = useRoute()
-
-const currentLayout = computed(() => route.meta.layout || 'default')
-
-const layoutMap = {
-  default: LayoutDefault,
-  auth: LayoutAuth
-}
-
 </script>
 
 <template>
