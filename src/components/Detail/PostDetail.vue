97611c56c12dccec3e5ba49a4817d08c145f4608
HaoNguyen-Ron <hao100319@gmail.com>
2024-04-09 10:54:41 +0700

    refactor/ PostDetail class name and css style

--- src/components/Detail/PostDetail.vue
+++ src/components/Detail/PostDetail.vue
@@ -2,7 +2,9 @@
 const tour = {
   title: ' Cần tuyển HDV',
   description: ' Chúng tôi đang cần HDV cho tour Đà Nẵng - Hội An - Huế cho đoàn gồm 5 khách Úc và 3 khách Đài Loan',
-  destinations: [
+  maximum_cv: 20,
+  price: 300,
+  total_destinations: [
     {
       id: 0,
       name: 'Đà Nẵng',
@@ -16,8 +18,7 @@ const tour = {
       name: 'Huế',
     },
   ],
-  pay_load: 300,
-  requirements: [
+  required_skill: [
     {
       id: 0,
       key: 'language',
@@ -34,7 +35,6 @@ const tour = {
       value: 12, // MONTH
     },
   ],
-  maximum_cv: 20,
   partner_attributes: [
     {
       id: 0,
@@ -75,96 +75,89 @@ function getLabel(attribute: {
 </script>
 
 <template>
-  <div :class="$style.post__detail__wrapper">
-    <div :class="$style.post__detail__header">
-      <h2 :class="$style['post__detail__header--title']">
+  <div :class="$style.postDetailWrapper">
+    <div :class="[$style.postDetailHeader, $style.postDetailBorderBottom]">
+      <h2 :class="$style.postDetailHeading">
         {{ tour.title }}
       </h2>
 
-      <p :class="$style['post__detail__header--time']" class="p-grey">
+      <p :class="$style.postDetailNote" class="mb-1">
         Đã đăng 1 giờ trước
       </p>
 
-      <div :class="$style['post__detail__header--descriptions']">
-        <div
-          v-for="destination in tour.destinations"
-          :key="`destination-${destination.id}`"
-          :class="$style.description__item"
+      <div :class="$style.postDetailList">
+        <span
+          v-for="destination in tour.total_destinations" :key="`destination-${destination.id}`"
+          :class="$style.postDetailItem"
         >
-          <div :class="$style.description__icon">
+          <div :class="$style.postDetailIconBlue">
             <i class="fa-solid fa-location-dot" />
           </div>
 
-          <div :class="$style.description__content">
+          <div :class="$style.postDetailContent">
             {{ destination.name }}
           </div>
-        </div>
+        </span>
       </div>
     </div>
 
-    <div :class="$style.post__detail__body">
-      <p :class="$style['post__detail__body--title']">
+    <div :class="$style.postDetailBody">
+      <p :class="[$style.postDetailTitle, $style.postDetailBorderBottom]">
         {{ tour.description }}
       </p>
 
-      <div :class="$style['post__detail__body--descriptions']">
-        <div :class="$style.description__item">
-          <div :class="$style.description__icon">
+      <div :class="$style.postDetailList">
+        <div :class="[$style.postDetailItem, $style.postDetailBorderBottom]">
+          <div :class="$style.postDetailIconGreen">
             <i class="fa-solid fa-money-bill" />
           </div>
 
-          <div :class="$style.description__content">
-            <div :class="$style['description__content--info']">
-              {{ handlePayLoad(tour.pay_load) }} / ngày
+          <div :class="$style.postDetailContent">
+            <div :class="$style.postDetailTitle" class="mb-1">
+              {{ handlePayLoad(tour.price) }} / ngày
             </div>
 
-            <div :class="$style['description__content--note']">
+            <div :class="$style.postDetailNote">
               Tiền công trên một ngày
             </div>
           </div>
         </div>
       </div>
 
-      <div :class="$style['post__detail__body--requirements']">
-        <h2 :class="$style.requirement__title">
+      <div :class="[$style.postDetailRequirement, $style.postDetailBorderBottom]">
+        <h2 :class="$style.postDetailHeading">
           Yêu cầu về trình độ
         </h2>
-        <div :class="$style.requirement__badges">
-          <div
-            v-for="requirement in tour.requirements"
-            :key="requirement.id"
-            :class="$style.badge"
-          >
-            {{ getLabel(requirement) }}
-          </div>
+
+        <div :class="$style.postDetailBagdeList">
+          <span v-for="skill in tour.required_skill" :key="skill.id" :class="$style.postDetailBagde">
+            {{ getLabel(skill) }}
+          </span>
         </div>
       </div>
 
-      <div :class="$style['post__detail__body--activities']">
-        <div :class="$style.activity__item">
-          <div :class="$style.activity__key">
+      <div :class="[$style.postDetailActivities, $style.postDetailBorderBottom]">
+        <div :class="$style.postDetailActivityList">
+          <div :class="$style.postDetailTitle">
             Tổng số ứng tuyển hiện tại:
           </div>
 
-          <div :class="$style.activity__value">
+          <div :class="$style.postDetailContent">
             2 trên {{ tour.maximum_cv }}
           </div>
         </div>
       </div>
 
-      <div :class="$style['post__detail__body--verifications']">
-        <h2 :class="$style.verifications__title">
+      <div :class="$style.postDetailVerification">
+        <h2 :class="$style.postDetailHeading">
           Thông tin về nhà tuyển dụng
         </h2>
 
-        <div :class="$style.verifications__content">
-          <div
-            v-for="attribute in tour.partner_attributes"
-            :key="attribute.id"
-            :class="$style.verifications__item"
-          >
-            <i v-if="attribute.status === 'verified'" class="fa-solid fa-square-check" style="color: green;" />
-            <i v-else class="fa-solid fa-circle-xmark" style="color: red;" />
+        <div :class="$style.postDetailList">
+          <div v-for="attribute in tour.partner_attributes" :key="attribute.id" :class="$style.postDetailItem">
+            <i v-if="attribute.status === 'verified'" class="fa-solid fa-square-check " :class="$style.postDetailIconGreen" />
+
+            <i v-else class="fa-solid fa-circle-xmark" :class="$style.postDetailIconRed" />
 
             <p>{{ attribute.key }} {{ attribute.status }}</p>
           </div>
@@ -172,12 +165,12 @@ function getLabel(attribute: {
       </div>
     </div>
 
-    <div :class="$style.post__detail__footer">
-      <button :class="[$style.post__detail__btn, $style['post__detail__btn--primary']]">
+    <div :class="$style.postDetailFooter">
+      <button :class="[$style.postDetailBtn, $style.postDetailBtnPrimary]">
         Nộp đơn
       </button>
 
-      <button :class="[$style.post__detail__btn, $style['post__detail__btn--outline']]">
+      <button :class="[$style.postDetailBtn, $style.postDetailBtnOutline]">
         Lưu bài viết
       </button>
     </div>
@@ -185,183 +178,133 @@ function getLabel(attribute: {
 </template>
 
 <style lang="scss" module>
-.post__detail__wrapper {
+.postDetailWrapper {
   background-color: white;
   padding: 2rem 1rem;
 
-  .post__detail__header {
-    padding: 2rem 0;
-    border-bottom: 0.1rem solid var(--color-primary-lighter);
-
-    &--title {
-      margin-bottom: 2rem;
-    }
-
-    &--time {
-      margin-bottom: 1rem;
-    }
-
-    &--descriptions {
-      .description__item {
-        display: flex;
-        align-items: start;
-        margin-bottom: 1rem;
-
-        &:last-child {
-          margin-bottom: 0;
-        }
-
-        .description__content {
-          font-weight: 600;
-        }
-
-        .description__icon {
-          margin-right: 1rem;
-          color: var(--color-primary)
-        }
-      }
-    }
-  }
-
-  .post__detail__body {
-
-    &--title {
-      padding: 2rem 0;
-      font-size: 1.5rem;
-      line-height: 3rem;
-      border-bottom: .1rem solid var(--color-primary-lighter);
-    }
-
-    &--descriptions {
-      padding: 2rem 0;
-      border-bottom: .1rem solid var(--color-primary-lighter);
-
-      .description__item {
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-
-        .description__icon {
-          margin-right: 1rem;
-          color: green
-        }
-
-        .description__content {
-          &--info {
-            margin-bottom: 1rem;
-          }
-
-          &--note {
-            font-size: 1.3rem;
-            color: var(--color-gray)
-          }
-        }
-      }
-    }
-
-    &--requirements {
-      padding: 2rem 0;
-      border-bottom: 0.1rem solid var(--color-primary-lighter);
-
-      .requirement__title {
-        margin-bottom: 1rem;
-      }
-
-      .requirement__badges {
-        display: flex;
-        gap: 1rem;
-
-        .badge {
-          display: inline-block;
-          background-color: var(--color-gray-light);
-          color: var(--color-gray-darker);
-          padding: 1rem 1rem;
-          border-radius: 2rem;
-        }
-      }
-    }
-
-    &--activities {
-      padding: 2rem 0;
-      border-bottom: .1rem solid var(--color-primary-lighter);
-
-      .activity__item {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-
-        .activity__value {
-          font-weight: 600;
-        }
-      }
-    }
-
-    &--verifications {
-      padding: 2rem 0;
-      border-bottom: .1rem solid var(--color-primary-lighter);
-
-      .verifications__title {
-        margin-bottom: 2rem;
-      }
-
-      .verifications__content {
-        display: flex;
-        flex-direction: column;
-        gap: 2rem;
-        .verifications__item {
-          display: flex;
-          gap: 1rem;
-
-          i.fa-square-check {
-            background-color: green;
-          }
-
-          i.fa-circle-xmark {
-            color: red;
-          }
-        }
-      }
-    }
-  }
-
-  .post__detail__footer {
-    background-color: white;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    gap: 1rem;
-    padding: 2rem 0;
-    width: 100%;
-
-    .post__detail__btn {
-      font-size: 2rem;
-      padding: 0.5rem 1rem;
-      border-radius: 9999px;
-      outline: none;
-      cursor: pointer;
-      transition-duration: 300ms;
-
-      &:hover {
-        color: white;
-        background-color: var(--color-primary-dark);
-      }
-
-      &:active {
-        color: white;
-        background-color: var(--color-primary-darker);
-      }
-
-      &.post__detail__btn--primary {
-        border: none;
-        background-color: var(--color-primary);
-        color: white;
-      }
-
-      &.post__detail__btn--outline {
-        border: 1px solid var(--color-primary);
-        color: var(--color-primary);
-        background-color: white;
-      }
-    }
-  }
 }
+
+.postDetailHeader {
+  padding: 2rem 0;
+  border-bottom: 0.1rem solid var(--color-primary-lighter);
+}
+
+.postDetailFooter {
+  background-color: white;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  padding: 2rem 0;
+  width: 100%;
+}
+
+.postDetailList {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.postDetailItem {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+
+.postDetailRequirement {
+  padding: 2rem 0;
+}
+
+.postDetailBagdeList {
+  display: flex;
+  gap: 1rem;
+}
+
+.postDetailBagde {
+  display: inline-block;
+  background-color: var(--color-gray-light);
+  color: var(--color-gray-darker);
+  padding: 1rem 1rem;
+  border-radius: 2rem;
+}
+
+.postDetailActivities {
+  padding: 2rem 0;
+}
+
+.postDetailActivityList {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.postDetailVerification {
+  padding: 2rem 0;
+}
+
+.postDetailHeading {
+  font-weight: 600;
+  font-size: 1.8rem;
+  margin-bottom: 2rem;
+}
+
+.postDetailContent {
+  font-weight: 600;
+}
+
+.postDetailTitle {
+  font-size: 1.5rem;
+  line-height: 2.5rem;
+}
+
+.postDetailNote {
+  color: var(--color-gray);
+}
+
+.postDetailIconBlue {
+  color: var(--color-primary-darker);
+}
+
+.postDetailIconGreen {
+  color: green;
+}
+
+.postDetailIconRed {
+  color: red;
+}
+
+.postDetailBorderBottom {
+  padding: 2rem 0;
+  border-bottom: .1rem solid var(--color-primary-lighter);
+}
+
+.postDetailBtn {
+    font-size: 2rem;
+    padding: 0.5rem 1rem;
+    border-radius: 9999px;
+    outline: none;
+    cursor: pointer;
+    transition-duration: 300ms;
+
+    &:hover {
+      color: white;
+      background-color: var(--color-primary-dark);
+    }
+
+    &:active {
+      color: white;
+      background-color: var(--color-primary-darker);
+    }
+
+  }
+  .postDetailBtnPrimary {
+    border: none;
+    background-color: var(--color-primary);
+    color: white;
+  }
+
+  .postDetailBtnOutline {
+    border: 1px solid var(--color-primary);
+    color: var(--color-primary);
+    background-color: white;
+  }
 </style>
